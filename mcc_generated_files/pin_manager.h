904d6a094a11b4c16405f463e62892dfe0454fd9
hugoalanochoa <hugoalanochoa@yahoo.com.mx>
2017-06-13 11:03:54 -0600

    Initial commit

---
+++ mcc_generated_files/pin_manager.h
@@ -0,0 +1,192 @@
+/**
+  @Generated Pin Manager Header File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the Pin Manager file generated using MPLAB(c) Code Configurator
+
+  @Description:
+    This header file provides implementations for pin APIs for all pins selected in the GUI.
+    Generation Information :
+        Product Revision  :  MPLAB(c) Code Configurator - 4.15.1
+        Device            :  PIC18F26K80
+        Version           :  1.01
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 1.35
+        MPLAB             :  MPLAB X 3.40
+
+    Copyright (c) 2013 - 2015 released Microchip Technology Inc.  All rights reserved.
+
+    Microchip licenses to you the right to use, modify, copy and distribute
+    Software only when embedded on a Microchip microcontroller or digital signal
+    controller that is integrated into your product or third party product
+    (pursuant to the sublicense terms in the accompanying license agreement).
+
+    You should refer to the license agreement accompanying this Software for
+    additional information regarding your rights and obligations.
+
+    SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+    EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+    MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+    IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+    CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+    OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+    INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+    CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+    SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+    (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+*/
+
+
+#ifndef PIN_MANAGER_H
+#define PIN_MANAGER_H
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set IO_RB0 aliases
+#define IO_RB0_TRIS               TRISBbits.TRISB0
+#define IO_RB0_LAT                LATBbits.LATB0
+#define IO_RB0_PORT               PORTBbits.RB0
+#define IO_RB0_WPU                WPUBbits.WPUB0
+#define IO_RB0_ANS                ANCON1bits.ANSEL10
+#define IO_RB0_SetHigh()            do { LATBbits.LATB0 = 1; } while(0)
+#define IO_RB0_SetLow()             do { LATBbits.LATB0 = 0; } while(0)
+#define IO_RB0_Toggle()             do { LATBbits.LATB0 = ~LATBbits.LATB0; } while(0)
+#define IO_RB0_GetValue()           PORTBbits.RB0
+#define IO_RB0_SetDigitalInput()    do { TRISBbits.TRISB0 = 1; } while(0)
+#define IO_RB0_SetDigitalOutput()   do { TRISBbits.TRISB0 = 0; } while(0)
+#define IO_RB0_SetPullup()      do { WPUBbits.WPUB0 = 1; } while(0)
+#define IO_RB0_ResetPullup()    do { WPUBbits.WPUB0 = 0; } while(0)
+#define IO_RB0_SetAnalogMode()  do { ANCON1bits.ANSEL10 = 1; } while(0)
+#define IO_RB0_SetDigitalMode() do { ANCON1bits.ANSEL10 = 0; } while(0)
+
+// get/set RB1 procedures
+#define RB1_SetHigh()    do { LATBbits.LATB1 = 1; } while(0)
+#define RB1_SetLow()   do { LATBbits.LATB1 = 0; } while(0)
+#define RB1_Toggle()   do { LATBbits.LATB1 = ~LATBbits.LATB1; } while(0)
+#define RB1_GetValue()         PORTBbits.RB1
+#define RB1_SetDigitalInput()   do { TRISBbits.TRISB1 = 1; } while(0)
+#define RB1_SetDigitalOutput()  do { TRISBbits.TRISB1 = 0; } while(0)
+#define RB1_SetPullup()     do { WPUBbits.WPUB1 = 1; } while(0)
+#define RB1_ResetPullup()   do { WPUBbits.WPUB1 = 0; } while(0)
+#define RB1_SetAnalogMode() do { ANCON1bits.ANSEL8 = 1; } while(0)
+#define RB1_SetDigitalMode()do { ANCON1bits.ANSEL8 = 0; } while(0)
+
+// get/set RB2 procedures
+#define RB2_SetHigh()    do { LATBbits.LATB2 = 1; } while(0)
+#define RB2_SetLow()   do { LATBbits.LATB2 = 0; } while(0)
+#define RB2_Toggle()   do { LATBbits.LATB2 = ~LATBbits.LATB2; } while(0)
+#define RB2_GetValue()         PORTBbits.RB2
+#define RB2_SetDigitalInput()   do { TRISBbits.TRISB2 = 1; } while(0)
+#define RB2_SetDigitalOutput()  do { TRISBbits.TRISB2 = 0; } while(0)
+#define RB2_SetPullup()     do { WPUBbits.WPUB2 = 1; } while(0)
+#define RB2_ResetPullup()   do { WPUBbits.WPUB2 = 0; } while(0)
+
+// get/set RB3 procedures
+#define RB3_SetHigh()    do { LATBbits.LATB3 = 1; } while(0)
+#define RB3_SetLow()   do { LATBbits.LATB3 = 0; } while(0)
+#define RB3_Toggle()   do { LATBbits.LATB3 = ~LATBbits.LATB3; } while(0)
+#define RB3_GetValue()         PORTBbits.RB3
+#define RB3_SetDigitalInput()   do { TRISBbits.TRISB3 = 1; } while(0)
+#define RB3_SetDigitalOutput()  do { TRISBbits.TRISB3 = 0; } while(0)
+#define RB3_SetPullup()     do { WPUBbits.WPUB3 = 1; } while(0)
+#define RB3_ResetPullup()   do { WPUBbits.WPUB3 = 0; } while(0)
+
+// get/set STA_LED aliases
+#define STA_LED_TRIS               TRISBbits.TRISB5
+#define STA_LED_LAT                LATBbits.LATB5
+#define STA_LED_PORT               PORTBbits.RB5
+#define STA_LED_WPU                WPUBbits.WPUB5
+#define STA_LED_SetHigh()            do { LATBbits.LATB5 = 1; } while(0)
+#define STA_LED_SetLow()             do { LATBbits.LATB5 = 0; } while(0)
+#define STA_LED_Toggle()             do { LATBbits.LATB5 = ~LATBbits.LATB5; } while(0)
+#define STA_LED_GetValue()           PORTBbits.RB5
+#define STA_LED_SetDigitalInput()    do { TRISBbits.TRISB5 = 1; } while(0)
+#define STA_LED_SetDigitalOutput()   do { TRISBbits.TRISB5 = 0; } while(0)
+#define STA_LED_SetPullup()      do { WPUBbits.WPUB5 = 1; } while(0)
+#define STA_LED_ResetPullup()    do { WPUBbits.WPUB5 = 0; } while(0)
+
+// get/set SS aliases
+#define SS_TRIS               TRISCbits.TRISC2
+#define SS_LAT                LATCbits.LATC2
+#define SS_PORT               PORTCbits.RC2
+#define SS_SetHigh()            do { LATCbits.LATC2 = 1; } while(0)
+#define SS_SetLow()             do { LATCbits.LATC2 = 0; } while(0)
+#define SS_Toggle()             do { LATCbits.LATC2 = ~LATCbits.LATC2; } while(0)
+#define SS_GetValue()           PORTCbits.RC2
+#define SS_SetDigitalInput()    do { TRISCbits.TRISC2 = 1; } while(0)
+#define SS_SetDigitalOutput()   do { TRISCbits.TRISC2 = 0; } while(0)
+
+// get/set RC3 procedures
+#define RC3_SetHigh()    do { LATCbits.LATC3 = 1; } while(0)
+#define RC3_SetLow()   do { LATCbits.LATC3 = 0; } while(0)
+#define RC3_Toggle()   do { LATCbits.LATC3 = ~LATCbits.LATC3; } while(0)
+#define RC3_GetValue()         PORTCbits.RC3
+#define RC3_SetDigitalInput()   do { TRISCbits.TRISC3 = 1; } while(0)
+#define RC3_SetDigitalOutput()  do { TRISCbits.TRISC3 = 0; } while(0)
+
+// get/set RC4 procedures
+#define RC4_SetHigh()    do { LATCbits.LATC4 = 1; } while(0)
+#define RC4_SetLow()   do { LATCbits.LATC4 = 0; } while(0)
+#define RC4_Toggle()   do { LATCbits.LATC4 = ~LATCbits.LATC4; } while(0)
+#define RC4_GetValue()         PORTCbits.RC4
+#define RC4_SetDigitalInput()   do { TRISCbits.TRISC4 = 1; } while(0)
+#define RC4_SetDigitalOutput()  do { TRISCbits.TRISC4 = 0; } while(0)
+
+// get/set CEN aliases
+#define CEN_TRIS               TRISCbits.TRISC6
+#define CEN_LAT                LATCbits.LATC6
+#define CEN_PORT               PORTCbits.RC6
+#define CEN_SetHigh()            do { LATCbits.LATC6 = 1; } while(0)
+#define CEN_SetLow()             do { LATCbits.LATC6 = 0; } while(0)
+#define CEN_Toggle()             do { LATCbits.LATC6 = ~LATCbits.LATC6; } while(0)
+#define CEN_GetValue()           PORTCbits.RC6
+#define CEN_SetDigitalInput()    do { TRISCbits.TRISC6 = 1; } while(0)
+#define CEN_SetDigitalOutput()   do { TRISCbits.TRISC6 = 0; } while(0)
+
+/**
+   @Param
+    none
+   @Returns
+    none
+   @Description
+    GPIO and peripheral I/O initialization
+   @Example
+    PIN_MANAGER_Initialize();
+ */
+void PIN_MANAGER_Initialize (void);
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    Interrupt on Change Handling routine
+ * @Example
+    PIN_MANAGER_IOC();
+ */
+void PIN_MANAGER_IOC(void);
+
+
+
+#endif // PIN_MANAGER_H
+/**
+ End of File
+*/
